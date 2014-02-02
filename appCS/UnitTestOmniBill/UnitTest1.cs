67adf085b85ff60a8b96a0bb922e1482aff21569
Alex Shkunov <toman22@yandex.ru>
2014-02-02 11:18:32 +0200

    Create WPF app + unit test project

---
+++ appCS/UnitTestOmniBill/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using omniBill;
+
+namespace UnitTestOmniBill
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
