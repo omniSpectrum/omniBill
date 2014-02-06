1d10415eb836f7b3279a6302ab4d8c6dcd48ac4b
Alex Shkunov <toman22@yandex.ru>
2014-02-06 19:32:38 +0200

    Create GenericModel + Create Interface IDataAcessHandler

---
+++ appCS/UnitTestOmniBill/DataAcessDBTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using omniBill;
+//using omniBill.InnerComponents.DataAcessLayer;
+//using omniBill.InnerComponents.Interfaces;
+
+namespace UnitTestOmniBill
+{
+    [TestClass]
+    public class DataAcessDBTests
+    {
+        [TestMethod]
+        public void TestConnectionString()
+        {
+            //IDataAcessHandler db = new DatabaseAcessHandler();
+        }
+    }
+}
