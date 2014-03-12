42c1fcb2a47495f0d3ff12c24f8d90ddef571c38
Ytseboy <danfreitaschn+programming@gmail.com>
2014-03-12 15:05:50 +0200

    Added two model classes

---
+++ appCS/omniBill/InnerComponents/Models/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace omniBill.InnerComponents.Models
+{
+    public class Item: BaseModel
+    {
+        public Item() : base(0) { }
+
+        public Item(int itemId, string itemName, string descr, decimal price, VatGroup vatId) : base(itemId) 
+        {
+            ItemName = itemName;
+            Descr = descr;
+            Price = price;
+            VatId = vatId;
+        }
+
+        public string ItemName { get; set; }
+        public string Descr { get; set; }
+        public decimal Price { get; set; }
+        public VatGroup VatId { get; set; }
+    }
+}
---
+++ appCS/omniBill/InnerComponents/Models/VatGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace omniBill.InnerComponents.Models
+{
+    public class VatGroup: BaseModel
+    {
+        public VatGroup(int vatId, double percentage) : base(vatId)
+        {
+            Percentage = percentage;
+        }
+
+        public double Percentage { get; set; }
+    }
+}
