8d8168e75b6f3266209a8b14fc2b7d9bb5234e07
Ytseboy <danfreitaschn+programming@gmail.com>
2014-04-07 12:53:29 +0300

    UI Updatw

--- appCS/omniBill/MainWindow.xaml.cs
+++ appCS/omniBill/MainWindow.xaml.cs
@@ -22,5 +22,13 @@ namespace omniBill
         {
             InitializeComponent();
         }
+
+        private void settingsDropDownButton_Click(object sender, RoutedEventArgs e)
+        {
+            (sender as Button).ContextMenu.IsEnabled = true;
+            (sender as Button).ContextMenu.PlacementTarget = (sender as Button);
+            (sender as Button).ContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+            (sender as Button).ContextMenu.IsOpen = true;
+        }
     }
 }
